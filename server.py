d80710436491d2a38933302363b9ee0e2c61cf72
texdade <davidetessarolo@gmail.com>
2020-04-16 11:10:52 +0200

    Started webscraping, nothing is working properly

--- server.py
+++ server.py
@@ -1,9 +1,30 @@
 from bot import gamesBot
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+def addNewGame(newGames):
+    #set up webdriver to fetch page
+    options = webdriver.ChromeOptions()
+    options.binary_location = "/usr/bin/google-chrome-stable"
+    chrome_driver_binary = "./chromedriver"
+    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+    driver.get("https://steamdb.info/upcoming/free/")
+    content = driver.page_source
+
+    #set up soup for webscraping
+    soup = BeautifulSoup(content, features="html.parser")
+
+    #search for interesting part
+    promotions = soup.find_all('tr', class_='app sub')
+
+    print(promotions)
+
 
 while True:
-    print("Idle...")
-
-    newGames = None #modificare dopo plz
+    newGames = []
+    notifiedGames = []
+    addNewGame(newGames)
 
     if(newGames):
         for items in newGames:
@@ -11,7 +32,10 @@ while True:
                 msg = forgeBotMsg(items)
             except:
                 msg = None
-            if msg is not None:
-                bot.sendMessage()
+            #if msg is not None:
+                #gamesBot.sendMessage()
+    
+    print("Idling one hour...")
+    time.sleep(3600)
             
 
